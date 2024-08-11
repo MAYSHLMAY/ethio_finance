318d1d8d22279a66f491260c4c06f3b0cacb2059
Mikiyas dawit <educationforlife424@gmail.com>
2024-08-11 15:39:27 +0300

    config files added

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCeJntn40p7yvp1d77raiMW3S_1Mw1dN7Y",
-  authDomain: "finance-tracker-app-1128c.firebaseapp.com",
-  projectId: "finance-tracker-app-1128c",
-  storageBucket: "finance-tracker-app-1128c.appspot.com",
-  messagingSenderId: "90641179227",
-  appId: "1:90641179227:web:3d4a0e6af98ed3bce7d57e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
