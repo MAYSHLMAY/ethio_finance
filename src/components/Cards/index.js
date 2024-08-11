17fb263322cc2996c3e62b1a731a5b819bb32ccc
Mikiyas dawit <educationforlife424@gmail.com>
2024-08-11 15:29:56 +0300

    done

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -13,18 +13,18 @@ const Cards = ({
       <Row className="card-row container">
         <Card className="mycard current-balance">
           <h2 className="title">Current Balance</h2>
-          <p>₹ {currentBalance}</p>
+          <p>{currentBalance} ETB</p>
         </Card>
         <Card className="mycard">
           <h2 className="title">Total Income</h2>
-          <p>₹ {income}</p>
+          <p> {income} ETB</p>
           <Button className="btn reset-balance-btn" onClick={showIncomeModal}>
             Add Income
           </Button>
         </Card>
         <Card className="mycard">
           <h2 className="title">Total Expenses</h2>
-          <p>₹ {expense}</p>
+          <p>{expense} ETB</p>
           <Button className="btn reset-balance-btn" onClick={showExpenseModal}>
             Add Expense
           </Button>
